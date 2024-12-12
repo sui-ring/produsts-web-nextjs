d65195b7ca0ae84d285e6069937a13bd79c021a1
go-numb <gonumb66@gmail.com>
2024-12-12 19:34:46 +0900

    add google search tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ import { SITENAME, SITEDESCRIPTION } from "./const";
 export const metadata: Metadata = {
   title: SITENAME,
   description: SITEDESCRIPTION,
+  verification: {
+    // <meta name="google-site-verification" content="eONIAiWLaZ_HYxHcKTUBuw8KDWH0SDyckup2RPcP4D0" />
+    google: "eONIAiWLaZ_HYxHcKTUBuw8KDWH0SDyckup2RPcP4D0",
+  },
 };
 
 export default function RootLayout({
